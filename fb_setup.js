650612bee6f301c3138d7250c2f753dd80a4752d
26360jl-cloud <26360jl@hvhs.school.nz>
2026-02-05 13:02:17 +1300

    2

---
+++ fb_setup.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAboa585vFNBApD40DNOiPatFOwumthHD4",
+  authDomain: "comp-5e4a5.firebaseapp.com",
+  databaseURL: "https://comp-5e4a5-default-rtdb.firebaseio.com",
+  projectId: "comp-5e4a5",
+  storageBucket: "comp-5e4a5.firebasestorage.app",
+  messagingSenderId: "54958349244",
+  appId: "1:54958349244:web:e9365af72e18efb8e929b9"
+};
